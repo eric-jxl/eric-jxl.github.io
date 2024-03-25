4029eef648b0dea7925385e8bcce0e8fc5a2f705
Eric <jxleric95@gmail.com>
2024-03-25 16:54:03 +0800

    修改js
    Signed-off-by: Eric <jxleric95@gmail.com>

--- js/timer.js
+++ js/timer.js
@@ -123,3 +123,27 @@ function time(obj, futimg) {
 
 	return true;
 }
+
+function getData(url) {
+	return new Promise((resolve, reject) => {
+		fetch(url)
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(`HTTP error! status: ${response.status}`);
+				}
+				return response.json();
+			})
+			.then((data) => {
+				resolve(data);
+			})
+			.catch((error) => {
+				reject(error);
+			});
+	});
+}
+getData("https://api.vvhan.com/api/visitor.info").then((data) => {
+	console.log(data)
+}).catch((error) => {
+	console.error(error)
+})
+
